d3ce13953d5fcd96f885a461ee834ba27d288289
HARSH-MADHAVAN <harshmadhavan2003@gmail.com>
2024-09-08 01:09:45 +0530

    First Commit

---
+++ Searching/binarySearch.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+
+void init_code() {
+	fast_io;
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main(){
+	init_code();
+
+}
