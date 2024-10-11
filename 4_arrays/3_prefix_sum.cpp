1477d983e9b58c460bbf82740f501f3e6b6f88a8
HARSH-MADHAVAN <harshmadhavan2003@gmail.com>
2024-10-11 23:19:13 +0530

    Arrays

---
+++ 4_arrays/3_prefix_sum.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+/*
+	SAMPLE INPUT: -
+	9
+	-2 1 -3 4 -1 2 1 -5 4
+	
+*/
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    // Creating prefix array
+    int csum[n + 1] {0};
+    for (int i = 1; i < n+1; i++)
+    {
+        csum[i] = csum[i - 1] + arr[i - 1];
+    }
+
+    // printing prefix array
+    for (int i = 0; i < n+1; i++)
+    {
+        cout << csum[i] << " ";
+    }
+    
+    
+
+    return 0;
+}
