f6b03141dcf565453896d27e63806120a317fe95
HARSH-MADHAVAN <harshmadhavan2003@gmail.com>
2024-10-12 22:24:23 +0530

    Arrays

---
+++ 4_arrays/5_maximumSubarraySum_KadanesAlgo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int maximumSubarraySumKadane(int arr[], int n){
+
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int maximumSubarraySumKadane(arr, n);
+
+    cout << max << endl;
+
+    return 0;
+}
