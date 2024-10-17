7f2195f8d84d4c64a821569fcdf576631e809026
HARSH-MADHAVAN <harshmadhavan2003@gmail.com>
2024-10-17 14:07:54 +0530

    Arrays

---
+++ 4_arrays/10_arrayReverse_in_range.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int reverseArrayinRange(int arr[],int i,int j){
+    while(i<j){
+        swap(arr[i], arr[j]);
+        i++;
+        j--;
+    }
+}
+
+int main() {
+    int arr[]{10, 20, 30, 40, 50, 60};
+    int n = sizeof(arr) / sizeof(int);
+    int i = 2; //starting index
+    int j = 5; //ending index
+
+    reverseArrayinRange(arr, i, j); //swaping only between i and j
+
+    // output the modified array
+    for (int k = 0; k < n; k++)
+    {
+        cout << arr[k] << " ";
+    }
+    
+    return 0;
+}
--- 4_arrays/9_arrayReverse.cpp
+++ 4_arrays/9_arrayReverse.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 using namespace std;
+// number of steps = n/2;
+// Time complexity = O(n)
 
-int main() {
+int reverseArray(int arr[], int n){
+    int i = 0;
+    int j = n - 1;
+
+    while (i < j)
+    {
+        swap(arr[i], arr[j]);
+        i++;
+        j--;
+    }
     
+}
+int main() {
+    int arr[]{10, 20, 30, 40, 50, 60};
+    int n = sizeof(arr) / sizeof(int);
+
+    reverseArray(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i]<<" ";
+    }
+    cout << endl;
 
     return 0;
 }
